621b17009f09dc39f73fc3a72b3f4186e095c492
Abhijeet <abhijeet.goel@testaing.com>
2021-03-18 20:32:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+//<h4>{this.props.title}</h4>
+
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo: '',
+      todos: []
+    }
+
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    var mytodos = this.state.todos;
+    mytodos.push(this.state.todo);
+    this.setState({
+      todos: mytodos
+    });
+    console.log(this.state.todos);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form className="textInput" onSubmit={this.addTodo}>
+          <input
+            type="text"
+            name="name"
+            onChange={(event) => {
+              this.setState({
+                todo: event.target.value
+              });
+            }
+          }/> &nbsp;&nbsp;
+          <input type="submit" value="Add" />
+        </form>
+        <div className="todoList">
+          <ul>
+            {this.state.todos.map((todo) => <li>{todo}</li>)}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Index;
